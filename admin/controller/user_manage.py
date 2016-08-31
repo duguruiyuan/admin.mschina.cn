f7772ab25ce7d807c1622536732f595cba2ede2a
JsonWang <632494788@qq.com>
2016-08-31 23:46:22 +0800

    修改用户资料

--- admin/controller/user_manage.py
+++ admin/controller/user_manage.py
@@ -1,5 +1,5 @@
 # coding : utf-8
-from flask import render_template,request,json
+from flask import render_template,request,json,jsonify
 from . import ad
 import requests
 from flask_paginate import Pagination
@@ -35,4 +35,34 @@ def user_manage():
 @ad.route("/hs_manage")
 def hs_manage():
     return render_template("user/hs_manage.html")
+
+#编辑房源
+@ad.route("/user/do_update_userById",methods=['POST'])
+def do_update_userById():
+    user_id = request.json.get("user_id")
+    if not user_id:
+        return jsonify({"code": 0, "message": "参数错误"})
+    user_account = request.json.get("user_account")
+    user_password = request.json.get("user_password")
+    user_mobile = request.json.get("user_mobile")
+    user_type = request.json.get("user_type")
+
+    data = json.dumps({
+                       "user_id": user_id,
+                       "user_account": user_account,
+                       "user_password": user_password,
+                       "user_mobile": user_mobile,
+                       "user_type": user_type
+                       })
+    response = requests.put(Conf.API_ADDRESS+"/api/v1.0/update_userBaseById/"+user_id,
+                            data=data,
+                            headers={"content-type": "application/json"})
+    response_data = json.loads(response.content)
+    # code = 0编辑失败
+    if response_data["code"] == 0:
+        return jsonify(response_data)
+
+    # code = 1 编辑成功
+    if response_data["code"] == 1:
+        return jsonify({"code": 1, "message": "success"})
 
