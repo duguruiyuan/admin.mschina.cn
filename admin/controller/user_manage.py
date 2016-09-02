46f076eba6107479d023d81ca0a95c2cfeba7ae4
JsonWang <632494788@qq.com>
2016-09-02 09:37:20 +0800

    会员列表

--- admin/controller/user_manage.py
+++ admin/controller/user_manage.py
@@ -4,6 +4,7 @@ from . import ad
 import requests
 from flask_paginate import Pagination
 from admin.config import Conf
+import hashlib
 
 '''
 用户管理模块:包括普通用户管理和房东的管理
@@ -36,7 +37,7 @@ def user_manage():
 def hs_manage():
     return render_template("user/hs_manage.html")
 
-#编辑房源
+#更新用户资料
 @ad.route("/user/do_update_userById",methods=['POST'])
 def do_update_userById():
     user_id = request.json.get("user_id")
@@ -66,3 +67,53 @@ def do_update_userById():
     if response_data["code"] == 1:
         return jsonify({"code": 1, "message": "success"})
 
+# 添加新用户
+#user_password_hash = hashlib.md5(user_password.encode('utf-8')).hexdigest()
+@ad.route("/user/insert_user",methods = ["POST"])
+def insert_user():
+    user_account = request.json.get("user_account")
+    if not user_account:
+        return jsonify({"code":0,"message":"用户名不能为空"})
+    user_password = request.json.get("user_password")
+    if not user_password:
+        return jsonify({"code": 0, "message": "密码不能为空"})
+
+    user_password_hash = hashlib.md5(user_password.encode('utf-8')).hexdigest()
+    user_mobile = request.json.get("user_mobile")
+    if not user_mobile:
+        return jsonify({"code": 0, "message": "手机号不能为空"})
+    user_headimg = request.json.get("user_headimg")
+    user_type = request.json.get("user_type")
+
+    data=json.dumps({
+        "user_account":user_account,
+        "user_password":user_password_hash,
+        "user_mobile":user_mobile,
+        "user_headimg":user_headimg,
+        "user_type":user_type
+    })
+    # data = json.dumps({"user_account": user_account, "user_password": user_password_hash})
+    response_data = requests.post(url=Conf.API_ADDRESS+"/api/v1.0/user_register", data = data, headers={"content-type": "application/json"})
+    result_data = json.loads(response_data.content)
+    #code =1 注册成功
+    if result_data['code'] == 1:
+        return jsonify('{"code":"1","message":"success"}')
+    #code = 0 注册失败
+    if response_data['code'] == 0:
+        return jsonify(result_data)
+    return jsonify(result_data)
+
+@ad.route("/user/delete_user_by_id",methods = ["POST"])
+def delete_user_by_id():
+    user_id = request.json.get("user_id")
+    if not user_id:
+        return jsonify({"code": 0, "message": "参数错误"})
+    response = requests.delete(Conf.API_ADDRESS + "/api/v1.0/delete_user_ById/" + str(user_id),headers={"content-type": "application/json"})
+    response_data = json.loads(response.content)
+    # code = 0删除失败
+    if response_data["code"] == 0:
+        return jsonify(response_data)
+
+    # code = 1 删除成功
+    if response_data["code"] == 1:
+        return jsonify({"code": 1, "message": "删除成功"})
